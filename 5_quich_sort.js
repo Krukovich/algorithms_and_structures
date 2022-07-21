8da0245d7e943bec6617721a3e5cee74ff45b210
Krukovich Alexey <aliaksei.krukovich@itechart-group.com>
2022-07-21 23:39:23 +0300

    feat: add another quickSort function

--- 5_quich_sort.js
+++ 5_quich_sort.js
@@ -1,7 +1,7 @@
-const arr = [0,3,2,5,6,8,23,9,4,2,1,2,9,6,4,1,7,-1, -5, 23,6,2,35,6,3,32,9,4,2,1,2,9,6,4,1,7,-1, -5, 23,9,4,2,1,2,9,6,4,1,7,-1, -5, 23,]
+const arr = [0,3,2,5,6,8,23,9,4,2,1,2,9,6,4,1,7,9,-1,-5,23,6,2,35,6,3,32,9,4,2,1,2,9,6,4,1,7,-1,-5,23,9,4,2,1,2,9,6,4,1,7,-1,-5,23,]
 let count = 0
 
-function quickSort(array) {
+const quickSort = (array) => {
     if (array.length <= 1) {
         return array
     }
@@ -22,5 +22,17 @@ function quickSort(array) {
     return [...quickSort(less), pivot, ...quickSort(greater)]
 }
 
+// Быстрая сортировка + уникальность
+const secondQuickSort = (arr) => {
+    if (arr.length < 2) {
+        return arr;
+    }
+    const midPoint = arr[Math.floor(arr.length / 2)]
+    const leftPart = arr.filter((item) => item < midPoint)
+    const rightPart = arr.filter((item) => item > midPoint)
+    return [...secondQuickSort(leftPart), midPoint, ...secondQuickSort(rightPart)]
+}
+
+console.log(secondQuickSort(arr))
 console.log(quickSort(arr))
 console.log('count', count)
